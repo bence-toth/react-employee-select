1ad9f5b96da0cb7bc424a11591929297bcd13f91
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-05-09 20:33:29 +0200

    Dealt with race conditions

--- client/src/app.consumer.js
+++ client/src/app.consumer.js
@@ -1,8 +1,8 @@
 const prepareQuery = query => query.split(' ').join('+')
 
-export default async ({pageLength, pageNumber, query, URL}) => {
+export default async ({pageLength, pageNumber, query, URL, fetchID}) => {
   const targetURL = URL
     || `http://localhost:3001/?per_page=${pageLength}&page=${pageNumber}&q=${prepareQuery(query)}`
   const response = await fetch(targetURL)
-  return response.json()
+  return {payload: response.json(), fetchID}
 }
--- client/src/app.container.js
+++ client/src/app.container.js
@@ -1,4 +1,4 @@
-import React, {useReducer, useEffect} from 'react'
+import React, {useReducer, useEffect, useRef} from 'react'
 
 import {reducer, initialState} from './app.reducer'
 import {clearSuggestions, updateQuery, selectEmployee} from './app.actionsCreators'
@@ -9,15 +9,41 @@ import Presenter from './app.presenter'
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  const fetchCounter = useRef(0)
+
+  const receiveEmployees = ({payload, fetchID}) => {
+    payload.then(data => {
+      if (fetchID === fetchCounter.current) {
+        receiveEmployeeData(dispatch)(data)
+      }
+    })
+  }
+
+  const getNewFetchID = () => { // Track the last fetch
+    const newFetchID = fetchCounter.current + 1
+    fetchCounter.current = newFetchID
+    return newFetchID
+  }
+
   useEffect(() => { // Fetch data as the user types
     dispatch(clearSuggestions())
-    fetchEmployees({pageLength: 6, pageNumber: 1, query: state.query})
-      .then(receiveEmployeeData(dispatch))
+    const newFetchID = getNewFetchID()
+    fetchEmployees({
+      pageLength: 6,
+      pageNumber: 1,
+      query: state.query,
+      fetchID: newFetchID
+    })
+      .then(receiveEmployees)
   }, [state.query])
 
-  const fetchNextPage = () => {
-    fetchEmployees({URL: state.nextPageURL})
-      .then(receiveEmployeeData(dispatch))
+  const fetchNextPage = () => { // Fetch next page when scrolled to bottom
+    const newFetchID = getNewFetchID()
+    fetchEmployees({
+      URL: state.nextPageURL,
+      fetchID: newFetchID
+    })
+      .then(receiveEmployees)
   }
 
   const fetchNextPageIfNeeded = () => {
