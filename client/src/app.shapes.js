c32fb8189950c5c756ff9ed2b526df65a1218fe2
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-05-09 20:44:48 +0200

    Fixed shape

--- client/src/app.shapes.js
+++ client/src/app.shapes.js
@@ -1,10 +1,12 @@
 import {string, shape} from 'prop-types'
 
 const employeeShape = shape({
-  name: string.isRequired,
-  id: string.isRequired,
-  email: string.isRequired,
-  avatar: string
+  attributes: {
+    name: string.isRequired,
+    id: string.isRequired,
+    email: string.isRequired,
+    avatar: string
+  }.isRequired
 })
 
 export {employeeShape}
